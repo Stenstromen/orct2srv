ff596ae18f0ca7bf5fb7d92d8ea5276305db91dc
Stenstromen <104073740+Stenstromen@users.noreply.github.com>
2023-03-12 18:53:31 +0100

    Removed env check

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-if [[ -z "$SAVEFILE" ]]; then
-  /OpenRCT2/openrct2-cli set-rct2 /orct2/Minimum
-  /OpenRCT2/openrct2-cli host $SAVEFILE --headless
-else
-  echo "Please set SAVEFILE environment variable to SC6 file"
-  exit 1
-fi
-
+/OpenRCT2/openrct2-cli set-rct2 /orct2/Minimum
+/OpenRCT2/openrct2-cli host $SAVEFILE --headless
